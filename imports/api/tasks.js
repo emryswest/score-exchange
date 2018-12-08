2a2421c0da66763fbb568d66c8a3b0ccf352a632
Emrys West <westemrys@gmail.com>
2018-12-08 15:41:47 -0700

    inital commit

---
+++ imports/api/tasks.js
@@ -0,0 +1,71 @@
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
+import { setSelectedUser, getSelectedUser } from '../ui/selecteduser';
+
+export const Tasks = new Mongo.Collection('tasks');
+
+
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish('tasks', function tasksPublication() {
+    return Tasks.find({
+      $or: [
+        { private: { $ne: true } },
+        { owner: this.userId },
+      ],
+    });
+  });
+}
+
+Meteor.methods({
+  // you have to specify the selectedUser so it gets sent to the server
+  'tasks.insert'(text,selectedUser) {
+
+    // Make sure the user is logged in before inserting a task
+    if (! this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+    user = selectedUser;
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: this.userId,
+      targetuser: user,
+      username: Meteor.users.findOne(this.userId).username,
+    });
+    setSelectedUser(undefined);
+  },
+  'tasks.remove'(taskId) {
+    check(taskId, String);
+
+    const task = Tasks.findOne(taskId);
+    
+
+    Tasks.remove(taskId);
+  },
+  'tasks.setChecked'(taskId, setChecked) {
+    check(taskId, String);
+    check(setChecked, Boolean);
+    const task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== this.userId) {
+      // If the task is private, make sure only the owner can check it off
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Tasks.update(taskId, { $set: { checked: setChecked } });
+  },
+  'tasks.setPrivate'(taskId, setToPrivate) {
+  check(taskId, String);
+  check(setToPrivate, Boolean);
+
+  const task = Tasks.findOne(taskId);
+
+  // Make sure only the task owner can make a task private
+  if (task.owner !== this.userId) {
+    throw new Meteor.Error('not-authorized');
+  }
+
+  Tasks.update(taskId, { $set: { private: setToPrivate } });
+},
+});
---
+++ imports/ui/App.jsx
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
+
+import { Tasks } from '../api/tasks.js';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
+import Task from './Task.js';
+import IndividualFile from './FileIndividualFile.js';
+import AccountsUIWrapper from './AccountsUIWrapper.js';
+import FileUpload from './FileUpload.js';
+import UserList from './UserList.js';
+import { getSelectedUser } from './selecteduser.js';
+
+  // App component - represents the whole app
+  class App extends Component {
+    constructor(props) {
+      super(props);
+
+    }
+
+    handleSubmit(event) {
+    event.preventDefault();
+
+    // Find the text field via the React ref
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Meteor.call('tasks.insert', text, getSelectedUser());
+
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
+
+
+    renderTasks() {
+      let myTasks = this.props.tasks;
+      let recentTask;
+    //  let index1;
+
+      // show those for US or for anyone!
+      myTasks = myTasks.filter(task =>
+        task.targetuser == Meteor.userId()
+      );
+      if (myTasks.length > 1) {
+        Meteor.call('tasks.remove', myTasks[0]._id);
+      }
+
+  //    myTasks = myTasks.reverse();
+
+      myTasks = myTasks.length > 1 ? [ myTasks[myTasks.length - 1] ] : myTasks;
+
+  //    index1 = myTasks.find[1];
+
+  //    console.log(index1);
+
+
+      return myTasks.map((task) => {
+          return (
+            <Task
+              key={task._id}
+              task={task}
+            />
+          );
+      });
+    }
+
+    passMyTasks() {
+      let passMyTasks = this.props.tasks;
+      passMyTasks = passMyTasks.filter(task =>
+      task.targetuser == Meteor.userId()
+      );
+      return (
+        passMyTasks
+      )
+    }
+
+    passAllTasks() {
+      let passAllTasks = this.props.tasks.username;
+      return (
+        passAllTasks
+      )
+    }
+
+  render() {
+let passMyTasks = this.passMyTasks();
+let passAllTasks = this.passAllTasks();
+
+    return (
+      <div>
+        <div className="app-container">
+        <header>
+        <AccountsUIWrapper />
+          <h1>Players</h1>
+
+        </header>
+          <UserList passMyTasks={passMyTasks} passAllTasks={passAllTasks}/>
+          <div className="material">
+            <ul>
+              {this.renderTasks()}
+            </ul>
+          </ div>
+
+          <FileUpload />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withTracker(() => {
+
+
+  return {
+    tasks: Tasks.find({}, { sort: { createdAt: 1 } }).fetch(),
+    incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
+    currentUser: Meteor.user(),
+  };
+})(App);
